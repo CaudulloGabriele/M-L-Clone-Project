d316154846491dc9a0a385c64d57ecf1cdbac5d2
GreenShark777 <105913446+CaudulloGabriele@users.noreply.github.com>
2022-05-25 17:27:51 +0200

    Update ShroobBehaviour.cs

--- Assets/Scripts/Enemies/ShroobBehaviour.cs
+++ Assets/Scripts/Enemies/ShroobBehaviour.cs
@@ -8,6 +8,14 @@ public class ShroobBehaviour : MonoBehaviour, IAmEnemy
     private EnemyTypesBehaviours etb;
 
 
+    private void Awake()
+    {
+        Debug.Log("CREATO SHROOB");
+
+        /*CHANGE STATS MULTIPLIERS FOR SHROOB*/
+
+    }
+
     #region Interface Methods
 
     public void SetEnemyTypesBehaviuorsRef(EnemyTypesBehaviours newEtb) { etb = newEtb; }
